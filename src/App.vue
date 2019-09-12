15232acebeeaa7785a063423ab1d4825cfe24c6f
panguanglian <919811760@qq.com>
2019-09-12 09:56:45 +0800

    pan

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+      <div class="nav">
+      <div class="logo">
+          <div>这是logo</div>
+          <div>商家入驻</div>
+      </div>
+      <div class="operation">
+          |<div>联系海露</div>
+          |<div>规则中心</div>
+          |<div>下载客户端</div>
+          <div>159998888</div>
+          |<div>退出</div>
+        
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +25,39 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  margin: 0;
 }
+.nav{
+        width: 90%;
+        height: 80px;
+        background: #2E7BEE;
+        display: flex;
+        padding: 0 5%;
+        align-items: center;
+        color: white;
+    }
+    .nav>div{
+        flex: 1;
+    }
+    .operation{
+        display: flex;
+        position: absolute;
+        right: 5%;
+    }
+    .operation>div{
+        
+         margin: 0 30px;
+         /* border-left: 1px solid white; */
+    }
+    .operation>div:last-child{
+        border: 1px solid white;
+        border-radius: 5px;
+        padding: 0 20px;
+       
+    }
+    .logo{
+      display: flex;
+    }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <div class="title">请选择您的店铺类型</div>
+    <div class="">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+        activeName: 'second'
+    };
+  },
+  methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title {
+  width: 100%;
+  text-align: center;
+  margin: 2% 0;
+  font-size: 18px;
+}
+</style>
