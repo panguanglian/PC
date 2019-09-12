b8ca5dd1c704aeac192155b892eb9d8a03d3b3e8
‘SupperScorpionMan’ <1141726687@qq.com>
2019-09-12 18:02:53 +0800

    today new change

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home">
+
     <div class="title">请选择您的店铺类型</div>
     <div class="genre">
       <el-tabs type="border-card">
         <el-tab-pane><div slot="label">个人店</div><div slot="label" class="aa">适合个人/个体工商店户入驻提供身份证等即可开店</div>
-          <div class="individual">
-            <div>个人店</div>
-            <div>个人身份开店（无营业执照）</div>
-          </div>
-          <div class="individual">
-            <div>个体店</div>
-            <div>个体工商户开店（有营业执照）</div>
+          <div class="individual" v-for="(item, i) in personal" :key="`personal_${ i }`" @click="_otherPage(item)">
+            <div>{{ item.title }}</div>
+            <div>{{ item.text }}</div>
           </div>
           <div class="btn">
             下一步
@@ -26,9 +23,26 @@
 export default {
   name: "home",
   data() {
-    return {};
+    return {
+      personal: [
+        {
+          title: '个人店',
+          text: '个人身份开店（无营业执照）',
+          link: 'individualBusiness'
+        },
+        {
+          title: '个人店',
+          text: '个人身份开店（有营业执照）',
+          link: 'personalStore'
+        }
+      ]
+    }
   },
-  methods: {}
+  methods: {
+    _otherPage(data) {
+      this.$router.push({ path: data.link })
+    }
+  }
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import home from '@/components/home/home'
+import individualBusiness from '@/components/home/individualBusiness'
+import personalStore from '@/components/home/personalStore'
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'home',
-        component: home
-    }]
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: home
+        },
+        {
+            path:'/personalStore',
+            name:'personalStore',
+            component: personalStore
+        },
+        {
+            path:'/individualBusiness',
+            name:'individualBusiness',
+            component: individualBusiness
+        }
+    ]
 
 })
